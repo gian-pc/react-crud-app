666bb92a1471124828242b0137ca523a1ebd778a
Gian Paucar <gpaucarcortez@gmail.com>
2022-04-06 19:24:36 -0500

    tercer-commit

--- src/components/CrudForm.js
+++ src/components/CrudForm.js
@@ -13,6 +13,15 @@ export const CrudForm = ({
   setDataToEdit,
 }) => {
   const [form, setForm] = useState(initialForm);
+
+  useEffect(() => {
+    if (dataToEdit) {
+      setForm(dataToEdit);
+    } else {
+      setForm(initialForm);
+    }
+  }, [dataToEdit]);
+
   const handleChange = (e) => {
     setForm({
       ...form,
@@ -43,7 +52,7 @@ export const CrudForm = ({
 
   return (
     <div>
-      <h3>Agregar</h3>
+      <h3>{dataToEdit ? "Editar" : "Agregar"}</h3>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
